f20b23f664dbb9987aefeef7a478d4b01fd2d5f3
Shane <6071159+smashedr@users.noreply.github.com>
2025-10-01 19:12:16 -0700

    Updates

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -e
 
 REGISTRY_HOST="ghcr.io"
-REGISTRY_USER="smashedr"
+REGISTRY_USER="cssnr"
 REGISTRY_REPO="docker-nginx-proxy"
 
 if [ -f ".env" ];then
@@ -13,11 +13,14 @@ fi
 
 if [ -z "${VERSION}" ];then
     if [ -z "${1}" ];then
-        read -rp "Version: " VERSION
+        read -rp "Version (latest): " VERSION
     else
         VERSION="${1}"
     fi
 fi
+
+[[ -z "${VERSION}" ]] && VERSION="latest"
+
 #if [ -z "${USERNAME}" ];then
 #    read -rp "Username: " USERNAME
 #fi
@@ -28,10 +31,14 @@ fi
 echo "${REGISTRY_HOST}/${REGISTRY_USER}/${REGISTRY_REPO}:${VERSION}"
 
 #docker login --username "${USERNAME}" --password "${PASSWORD}" "${REGISTRY_HOST}"
-docker login "${REGISTRY_HOST}"
+#docker login "${REGISTRY_HOST}"
 
-docker buildx create --use
-docker buildx build --platform linux/amd64,linux/arm64 --push \
-    -t "${REGISTRY_HOST}/${REGISTRY_USER}/${REGISTRY_REPO}:${VERSION}" nginx
+#docker buildx create --use
+#docker buildx build --platform linux/amd64,linux/arm64 \
+#    -t "${REGISTRY_HOST}/${REGISTRY_USER}/${REGISTRY_REPO}:${VERSION}" nginx
+
+docker build -t "${REGISTRY_HOST}/${REGISTRY_USER}/${REGISTRY_REPO}:${VERSION}" src
+
+#--build-arg NGINX_VERSION="${VERSION}" \
 
 #docker push "${REGISTRY_HOST}/${REGISTRY_USER}/${REGISTRY_REPO}:${VERSION}"
